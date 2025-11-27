674838fcf65c9f04b01e4fd783867b2ec9a567c2
Gagandeep Singh <142520245+gdsKaran@users.noreply.github.com>
2025-11-27 16:48:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,22 @@
 import React from "react";
 import './../styles/App.css';
 
+const weather = {
+  temp: 25,
+  conditions: "Sunny",
+};
+
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <>
+      <p>
+        <span style={weather.temp > 20 ? { color: "red" } : null}>
+          {weather.temp}
+        </span>
+      </p>
+      <p>{weather.conditions}</p>
+    </>
+  );
 }
 
 export default App
